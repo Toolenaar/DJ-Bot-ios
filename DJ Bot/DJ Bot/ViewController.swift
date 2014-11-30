e6aabdad57d9fe3a065388f1fe64d8b7b030f361
Jochem Toolenaar <toolenaar@gmail.com>
2014-11-30 13:35:30 +0100

    initial commit
    initial commit

---
+++ DJ Bot/DJ Bot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DJ Bot
+//
+//  Created by Jochem Toolenaar on 30/11/14.
+//  Copyright (c) 2014 JTMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
